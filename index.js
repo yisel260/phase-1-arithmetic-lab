07e734d7124dfc25e6825507d34dab5f7de502a0
Yisel Cintora Vega <yiselcintoravega@gmail.com>
2023-09-20 20:21:18 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
+let num1=31
+let num2 = 2 
+const multiply = num1 * num2
+const random = Math.floor(Math.random() * 10);
+let num3= 28
+let num4= 6
+const mod= num3 % num4
+const max = Math.max(1, 2, 13, 4, 20);
 // Write your code here
